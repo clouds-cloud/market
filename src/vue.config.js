2b1c27f4c6cc6f851e3f535d3f67586274a9399f
clouds-cloud <2574230242@qq.com>
2023-04-05 23:45:44 +0800

    test3

---
+++ src/vue.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    // import { resolve } from 'core-js/fn/promise';
+    configureWebpack: {
+        resolve: {
+            // alias: {
+            //     'assets': '@/assets',
+            //     'common': '@/common',
+            //     'components': '@/components',
+            //     'network': '@/network',
+
+            // }
+        }
+    }
+}
